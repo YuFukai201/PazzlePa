d5145def83d6705d99a9e831b9f3107ddc0b3185
YuFukai201 <yu.fukai0201@gmail.com>
2017-02-21 19:33:24 +0900

    全体で使える変数の管理処理

---
+++ InGame/common.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class common : MonoBehaviour {
+
+	//現在あるボールの数
+    //static public int all_ball;
+    //poseかそうでないか
+    static public bool mode_pose = false;
+    //時が止まっているかそうでないか
+    static public bool mode_Tstop = false;
+}
